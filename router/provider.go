4e7c0f3eacc755acff98c5826bb8c29e0c18f6bc
k-akari <tennis.akari.abcdefg@gmail.com>
2022-09-11 09:58:25 +0000

    prometheusでメトリクス取集する専用の/metricsを用意した。

--- router/provider.go
+++ router/provider.go
@@ -3,9 +3,12 @@ package router
 import (
 	"net/http"
 	"project/ent"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func RegisterRouter(mux *http.ServeMux, c *ent.Client) {
+	mux.Handle("/metrics", promhttp.Handler())
 	registerHealthCheckRouter(mux)
 	registerUserRouter(mux, c)
 }
